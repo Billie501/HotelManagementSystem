35f5ba7ca8e5445587d9ce1221f795d9a49084c4
Billie501 <132241423+Billie501@users.noreply.github.com>
2025-02-17 10:13:51 +1300

    Main hotel booking screen created

--- src/Hotel/Management/System/HotelManagementSystem.java
+++ src/Hotel/Management/System/HotelManagementSystem.java
@@ -4,6 +4,7 @@
  */
 package Hotel.Management.System;
 
+import java.awt.*;
 import javax.swing.*;
 
 /**
@@ -13,11 +14,46 @@ import javax.swing.*;
 public class HotelManagementSystem extends JFrame {
     
     HotelManagementSystem() {
-       setSize(1366, 565);
-       setLocation(100, 100);
+       //setSize(1366, 665);
+       //setLocation(100, 100);
+       setBounds(100, 100, 1366, 665);
+       setLayout(null);
+       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
        
+       ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/newfirstfirst.jpg"));
+       JLabel image = new JLabel(i1);
+       image.setBounds(0, 0, 1366, 665);
+       add(image);
        
+       JLabel text = new JLabel("HOTEL MANAGEMENT SYSTEM");
+       text.setBounds(180, 340, 1000, 90);
+       text.setForeground(Color.BLACK);
+       text.setFont(new Font("serif", Font.PLAIN, 50));
+       image.add(text);
+       
+       JButton next = new JButton("Next");
+       next.setBounds(1000, 360, 150, 50);
+       next.setBackground(Color.WHITE);
+       next.setForeground(Color.BLACK);
+       next.setFont(new Font("serif", Font.PLAIN, 24));
+       image.add(next);
+              
        setVisible(true);
+       
+       while(true) {
+           text.setVisible(false);
+           try {
+               Thread.sleep(500);
+           } catch (Exception e) {
+               e.printStackTrace();
+           }
+           text.setVisible(true);
+           try {
+               Thread.sleep(500);
+           } catch (Exception e) {
+               e.printStackTrace();
+           }
+       }
     }
     
     public static void main(String[] args) {
