0c4edb1c42437bf662ef718f154fdcb9053745ca
Billie501 <132241423+Billie501@users.noreply.github.com>
2025-02-17 09:07:54 +1300

    new hotel management system

---
+++ src/Hotel/Management/System/HotelManagementSystem.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Hotel.Management.System;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author billi
+ */
+public class HotelManagementSystem extends JFrame {
+    
+    HotelManagementSystem() {
+       setSize(1366, 565);
+       setLocation(100, 100);
+       
+       
+       setVisible(true);
+    }
+    
+    public static void main(String[] args) {
+        new HotelManagementSystem();
+        
+    }
+    
+}
