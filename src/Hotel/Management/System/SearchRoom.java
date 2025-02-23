93b7564df1dcaf5139beca9af21cbda414a49974
Billie501 <132241423+Billie501@users.noreply.github.com>
2025-02-24 12:16:58 +1300

    Incomplete Search Room form - bug present

---
+++ src/Hotel/Management/System/SearchRoom.java
@@ -0,0 +1,159 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Hotel.Management.System;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.*;
+import net.proteanit.sql.*;
+
+
+/**
+ *
+ * @author billi
+ */
+public class SearchRoom extends JFrame implements ActionListener{
+    
+    JTable table;
+    JButton submit, back;
+    JComboBox bedtype;
+    JCheckBox available;
+    
+    SearchRoom() {
+        
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+        
+        JLabel text = new JLabel("Search for Room");
+        text.setFont(new Font ("Tahoma", Font.PLAIN, 20));
+        text.setBounds(400, 30, 200, 30);
+        add(text);
+        
+        JLabel lblbed = new JLabel("Bed Type");
+        lblbed.setBounds(50, 100, 200, 30);
+        add(lblbed);
+        
+        bedtype = new JComboBox(new String[]{"Single King", "Double Queen"});
+        bedtype.setBounds(150, 100, 150, 25);
+        bedtype.setBackground(Color.WHITE);
+        add(bedtype);
+        
+        available = new JCheckBox("Only Display Available");
+        available.setBounds(650, 100, 150, 25);
+        available.setBackground(Color.WHITE);
+        add(available);
+        
+        JLabel l1 = new JLabel("Room Number");
+        l1.setBounds(50, 160, 100, 20);
+        add(l1);
+        
+        JLabel l2 = new JLabel("Availability");
+        l2.setBounds(270, 160, 100, 20);
+        add(l2);
+        
+        JLabel l3 = new JLabel("Cleaning Status");
+        l3.setBounds(450, 160, 100, 20);
+        add(l3);
+        
+        JLabel l4 = new JLabel("Price");
+        l4.setBounds(670, 160, 100, 20);
+        add(l4);
+        
+        JLabel l5 = new JLabel("Bed Type");
+        l5.setBounds(870, 160, 100, 20);
+        add(l5);
+        
+        table = new JTable();
+        table.setBounds(0, 200, 1000, 600);
+        add(table);
+        
+        try {
+            Conn c = new Conn();
+            ResultSet rs = c.s.executeQuery("select * from room");
+            table.setModel(DbUtils.resultSetToTableModel(rs));
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        submit = new JButton("Submit");
+        submit.setBackground(Color.BLACK);
+        submit.setForeground(Color.WHITE);
+        submit.addActionListener(this);
+        submit.setBounds(30, 520, 120,30);
+        add(submit);
+        
+        
+        back = new JButton("Back");
+        back.setBackground(Color.BLACK);
+        back.setForeground(Color.WHITE);
+        back.addActionListener(this);
+        back.setBounds(50, 520, 120,30);
+        add(back);
+        
+        setBounds(300, 200, 1050, 600);
+        setVisible(true);
+    }
+    
+    public void actionPerformed(ActionEvent ae) {
+        if(ae.getSource() == submit) {
+            try {
+                String query1 = "select * from room where bedtype = '"+bedtype.getSelectedItem()+"'";
+                
+                String query2 = "select * from room where availability = 'Available' AND bedtype = "+bedtype.getSelectedItem()+"'";
+                
+                Conn conn = new Conn();
+                ResultSet rs; 
+                
+                if(available.isSelected()) {
+                    rs = conn.s.executeQuery(query2);
+                } else {
+                    rs = conn.s.executeQuery(query1);
+                }
+                
+                table.setModel(DbUtils.resultSetToTableModel(rs));
+                
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+        setVisible(false);
+        new Reception();
+    }
+    
+//    public void actionPerformed(ActionEvent ae) {
+//    if (ae.getSource() == submit) {
+//        try {
+//            String query1 = "select * from room where bedtype = '"+bedtype.getSelectedItem()+"'";
+//            String query2 = "select * from room where availability = 'Available' AND bedtype = '"+bedtype.getSelectedItem()+"'";
+//
+//            Conn conn = new Conn();
+//            ResultSet rs;
+//
+//            if (available.isSelected()) {
+//                rs = conn.s.executeQuery(query2);
+//            } else {
+//                rs = conn.s.executeQuery(query1);
+//            }
+//
+//            table.setModel(DbUtils.resultSetToTableModel(rs));
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    } else if (ae.getSource() == back) {
+//        setVisible(false);
+//        new Reception();
+//    }
+//}
+
+    
+    public static void main(String[] args) {
+        new SearchRoom();
+    }
+    
+}
+
